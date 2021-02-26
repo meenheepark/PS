35b9624a0624e1393dd23f0248c2fe279d35a638
meenhee <mini971001@likelion.org>
2021-02-26 20:35:12 +0900

    first commit

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	//;_CRT_SECURE_NO_WARNINGS
+	freopen("input.txt", "r", stdin);
+	cin >> N;
+	cout << N;
+
+	return 0;
+}
